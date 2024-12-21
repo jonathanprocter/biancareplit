0799f72b2adedbab382528222c67b78d558b8054
Test User <test@example.com>
2024-12-21 20:53:10 +0000

    test: Add unformatted file to test Prettier

---
+++ client/src/test-format.ts
@@ -0,0 +1,11 @@
+export const unformattedFunction = (x: number, y: number) => {
+  return x + y;
+};
+
+const obj = {
+  foo: 'bar',
+  baz: 42,
+  qux: [1, 2, 3],
+};
+
+export const someArray = ['a', 'b', 'c'];
