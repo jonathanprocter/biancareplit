62e15244ae531bc8eeca1bb7aa7bec05aa44df52
Jonathan Procter <37925715-jonathanprocter@users.noreply.replit.com>
2024-12-21 21:18:37 +0000

    test: Fix ESLint warning in test file

--- client/src/test-format.ts
+++ client/src/test-format.ts
@@ -1,9 +1,11 @@
-export    const    unformattedFunction=(x:number,y:number)=>{return x+y};
-
-const obj={foo:'bar',
-  baz:     42,
-qux:[1,2,3]
+export const unformattedFunction = (x: number, y: number) => {
+  return x + y;
 };
 
-export const someArray=['a','b',
-'c'];
+export const formattedObject = {
+  foo: 'bar',
+  baz: 42,
+  qux: [1, 2, 3],
+};
+
+export const someArray = ['a', 'b', 'c'];
